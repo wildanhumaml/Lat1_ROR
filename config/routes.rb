6002cf07c014512e235025b09c3fbf05372cdc58
wildanhumaml <wildan@41studio.com>
2018-02-19 16:35:46 +0700

    First Commit Lat1

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :products
+  get 'say/hello'
+
+  get 'say/goodbye'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
