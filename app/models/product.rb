cc149f4a4cfadc2db255ffce7e2d7131e59759df
wildanhumaml <wildan@41studio.com>
2018-02-20 10:02:59 +0700

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,6 @@ class Product < ApplicationRecord
 	validates :title, uniqueness: true
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
-		message: 'must be a URL for GIF, JPG or PNG image .'
+		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
 end
