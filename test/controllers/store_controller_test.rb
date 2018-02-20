e27801f16992c370f3b59ca195526f5afcede15b
wildanhumaml <wildan@41studio.com>
2018-02-20 17:00:30 +0700

    Finish CatalogDisplay

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_sellect	'#columns #side a', minimum: 4
+    assert_sellect	'#main .entry', 3
+    assert_sellect	'h3', 'Programming Ruby 1.9'
+    assert_sellect	'.price', /\$[,\d]+\.\d\d/
   end
 
 end
