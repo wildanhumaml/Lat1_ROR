3deeac3060d4ee21ecde23b5a95477766d52e27e
wildanhumaml <wildan@41studio.com>
2018-02-22 11:37:35 +0700

    Check Out!

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
+
   get 'store/index'
 
-  resources :products
-  get 'say/hello'
+  resources :products do
+  	get :who_bought, on: :member
+  end
 
-  get 'say/goodbye'
+  # resources :products
+  # get 'say/hello'
+
+  # get 'say/goodbye'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
