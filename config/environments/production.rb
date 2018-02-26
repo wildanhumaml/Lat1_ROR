cbed204deaad6923be8814db00af6de5b50f2170
wildanhumaml <wildan@41studio.com>
2018-02-26 11:37:25 +0700

    Deployment and Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Dealing with log Files
+  config.logger = Logger.new(config.paths['log'].first, 'daily')
 end
